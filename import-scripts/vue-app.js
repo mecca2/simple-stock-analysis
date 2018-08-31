b6e75f91719c4dbfdc23b7d571bc5b43bdecb196
Danny Mecca <mecca2@clemson.edu>
2018-08-30 20:33:20 -0400

    Additional logic for searching stocks
    Added additional logic to show current stock price + list of stocks which had been previously searched.

--- import-scripts/vue-app.js
+++ import-scripts/vue-app.js
@@ -3,15 +3,25 @@
 const vm = new Vue({
   el: '.vue-app',
   data: {
-    results: [], 
+    results: {}, 
     companyInfo: false, 
     apiEndPoint : 'https://api.iextrading.com/1.0/', 
     stock: '', 
-    stocksSearched : []
+    stocksSearched : [], 
+    error: false, 
+    notFoundSku : '', 
+    apiRequestComplete: false
   },
-  mounted() {
+  mounted()  {
     this.init();
   }, 
+  computed: {
+    displaySearchedStocks: function(){
+      var tempString = this.stocksSearched.toString(); 
+      tempString = tempString.replace(',', ' , ');
+      return tempString; 
+    }
+  },
   methods : {
     init : function(){
       var self = this; 
@@ -22,12 +32,46 @@ const vm = new Vue({
     },
     submitStock: function(){
       var self = this; 
+      self.apiRequestComplete = false; 
       axios.get(this.apiEndPoint + '/stock/' + this.stock + '/company')
           .then(response => {
-            self.stocksSearched.push(this.stock); 
+            if(self.stocksSearched.indexOf(self.stock) == -1){
+              self.stocksSearched.unshift(self.stock); 
+            }
             self.companyInfo  = response.data;
-      })
-      console.log(self.stocksSearched); 
+            self.error = false; 
+          })
+          .catch(function (error) {
+            self.notFoundStock = self.stock;
+            self.error = true; 
+            console.log(self.notFoundStock);
+          });
+      // if no error then stock is found run rest of api requests 
+      if(!self.error){
+        self.getStockCurrentPrice(); 
+      }
+      
+    }, 
+    getStockCurrentPrice: function(){
+      var self = this; 
+      axios.get(this.apiEndPoint + '/stock/' + this.stock + '/price')
+          .then(response => {
+             
+            self.results.price = response.data; 
+            console.log('PRICE: ' + self.results.price);
+            self.apiRequestComplete = true; 
+          })
+          .catch(function (error) {
+            self.notFoundStock = self.stock;
+            self.error = true; 
+            console.log(self.notFoundStock);
+          });
+    }, 
+    formatPrice(value) {
+        if(typeof value !== "undefined"){
+          return "$" + value.toFixed(2); 
+        }
+        return "$" + value; 
     }
   }
 });
